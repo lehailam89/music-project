eb0b695bfa687cfec972d103198829aa8a6f2ce1
lehailam89 <lehailamltk@gmail.com>
2024-12-20 19:15:05 +0700

    Tạo controllers

--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,15 +1,7 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 const router = Router();
-import Topic from "../../models/topic.model";
+import * as topicController from "../../controllers/client/topic.controller";
 
-router.get("/", async (req: Request, res: Response) => {
-    const topics = await Topic.find({
-        deleted: false
-    });
-
-    res.render("client/pages/topics/index", {
-        topics
-    });
-});
+router.get("/", topicController.topics);
 
 export const topicRoutes: Router = router;
