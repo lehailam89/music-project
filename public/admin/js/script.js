6cc6ca8e9c6472ee7fb5bb6e9d89ccdbde6c63d4
lehailam89 <lehailamltk@gmail.com>
2025-01-05 11:13:59 +0700

    làm trang quản lý chủ đề(bên Admin)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const siderLinks = document.querySelectorAll(".sider .inner-menu ul li a");
-  
-    siderLinks.forEach(link => {
-      link.addEventListener("click", () => {
-        siderLinks.forEach(link => link.classList.remove("active"));
-        link.classList.add("active");
-      });
+  const siderLinks = document.querySelectorAll(".sider .inner-menu ul li a");
+
+  siderLinks.forEach(link => {
+    // Kiểm tra URL hiện tại và thêm lớp active vào thẻ a tương ứng
+    if (link.href === window.location.href) {
+      link.classList.add("active");
+    }
+
+    link.addEventListener("click", () => {
+      siderLinks.forEach(link => link.classList.remove("active"));
+      link.classList.add("active");
     });
   });
+});
